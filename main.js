dcee59d329c60bde9edd8fbf7c2582f204ea6a19
HAROLD <harold@Apple-Macbooks-MacBook-Pro-116.local>
2015-05-11 15:31:51 -0400

    intialcommit

---
+++ main.js
@@ -0,0 +1,47 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+//a:nothing
+
+
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// a: "Tim"
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// a: "JD"
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// a: "Hello, how are you?"
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+console.log (high - low );
+
+
+math = high - "5";
+// Q: What is `math` set to?
+console.log (high - "5" );
+
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+console.log ("Tim is " + (today - born -1) + "years old");
+
+
+
+// Store some information following in variables.
+var yourName = "Harold Oatneal";
+var instructorName = "JD";
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+console.log (statement);
